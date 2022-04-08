d28f358ab3fa5b55337c14fe0d22b1c4333b0a84
jsantis <san21153@uvg.edu.gt>
2022-04-08 12:02:10 -0600

    setting up the program

--- HDT7/src/hdt7/Operaciones.java
+++ HDT7/src/hdt7/Operaciones.java
@@ -4,10 +4,40 @@
  */
 package hdt7;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author jsken
  */
 public class Operaciones {
+    Archivo read = new Archivo();
+    ArrayList<String> lineas = new ArrayList<String>();
+
+    public void settingUp() throws IOException, FileNotFoundException {
+        ArrayList <String> Datos = new ArrayList<>();
+        try {
+            Datos = read.leerlineas();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < Datos.size(); i++) {
+            String[] palabras = Datos.get(i).split(",");
+            for (int j = 0; j < palabras.length; j++) {
+                palabras[j] = palabras[j].toLowerCase();
+                lineas.add(palabras[j]);
+            }
+        }
+    }
+
     
+    
+
 }
