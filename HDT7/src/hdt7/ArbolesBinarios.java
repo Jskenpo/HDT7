f9e437199eddc3bb5f490f79205b1e1a327a41d0
jsantis <san21153@uvg.edu.gt>
2022-04-08 12:58:24 -0600

    update Nodes
    - Busqueda de nodos completados
    - Eliminiación de nodos completados
    - Imprimir el arbol completado

--- HDT7/src/hdt7/ArbolesBinarios.java
+++ HDT7/src/hdt7/ArbolesBinarios.java
@@ -19,6 +19,11 @@ public class ArbolesBinarios<k,V> {
 
 
     //insert new node in a binary tree
+    /**
+     *
+     * @param key
+     * @param value
+     */
     public void insert(k key, V value) {
         Nodo newNode = new Nodo(key);
         newNode.value = value;
@@ -59,7 +64,99 @@ public class ArbolesBinarios<k,V> {
                 return null;
             }
         }
-        return focusNode.value;
+        return null;
     }
+
+    // delete a node from the binary tree
+
+    public void delete(k key) {
+        Nodo focusNode = root;
+        Nodo parent = root;
+        boolean isLeftChild = true;
+        while (focusNode.key != key) {
+            parent = focusNode;
+            if (((String) key ).compareTo((String)focusNode.key) < 0) {
+                isLeftChild = true;
+                focusNode = focusNode.left;
+            } else {
+                isLeftChild = false;
+                focusNode = focusNode.right;
+            }
+            if (focusNode == null) {
+                return;
+            }
+        }
+        if (focusNode.left == null && focusNode.right == null) {
+            if (focusNode == root) {
+                root = null;
+            }
+            if (isLeftChild == true) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (focusNode.right == null) {
+            if (focusNode == root) {
+                root = focusNode.left;
+            } else if (isLeftChild) {
+                parent.left = focusNode.left;
+            } else {
+                parent.right = focusNode.left;
+            }
+        } else if (focusNode.left == null) {
+            if (focusNode == root) {
+                root = focusNode.right;
+            } else if (isLeftChild) {
+                parent.left = focusNode.right;
+            } else {
+                parent.right = focusNode.right;
+            }
+        } else {
+            Nodo replacement = getReplacementNode(focusNode);
+            if (focusNode == root) {
+                root = replacement;
+            } else if (isLeftChild) {
+                parent.left = replacement;
+            } else {
+                parent.right = replacement;
+            }
+            replacement.left = focusNode.left;
+        }
+    }
+
+    //get the replacement node for the node to be deleted
+    public Nodo getReplacementNode(Nodo replacedNode) {
+        Nodo replacementParent = replacedNode;
+        Nodo replacement = replacedNode;
+        Nodo focusNode = replacedNode.right;
+        while (focusNode != null) {
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.left;
+        }
+        if (replacement != replacedNode.right) {
+            replacementParent.left = replacement.right;
+            replacement.right = replacedNode.right;
+        }
+        return replacement;
+    }
+
+    // print the tree inorder
+    public void printTree() {
+        if (root == null) {
+            System.out.println("Tree is empty");
+        } else {
+            printTree(root);
+        }
+    }
+
+    public void printTree(Nodo focusNode) {
+        if (focusNode != null) {
+            printTree(focusNode.left);
+            System.out.println(focusNode.key + " " + focusNode.value);
+            printTree(focusNode.right);
+        }
+    }
+
     
 }
