0a2f48c788e11fa4489ddbce9d069b0f013da806
jsantis <san21153@uvg.edu.gt>
2022-04-08 11:17:08 -0600

    corrección de lectura de archivos

--- HDT7/src/hdt7/Archivo.java
+++ HDT7/src/hdt7/Archivo.java
@@ -4,30 +4,55 @@
  */
 package hdt7;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 /**
  *
  * @author jsken
  */
 public class Archivo {
-    //crear método que lea un archivo linea por linea y lo guarde en un arraylist
+    
+    public ArrayList<String> leerlineas() throws IOException, FileNotFoundException{
+        // pedir al usuario que ingrese la dirección del archivo
+        System.out.println("Ingrese la dirección del archivo");
+        String ruta = System.console().readLine();
+        ArrayList<String> lineas = new ArrayList<String>();
+        // leer el archivo
+        try{
+        FileReader fr = new FileReader(ruta);
+        BufferedReader br = new BufferedReader(fr);
+        
+        // leer linea por linea
+        String linea = br.readLine();
+        while(linea != null){
+            System.out.println(linea);
+            linea = br.readLine();
 
-    public static ArrayList<String> leerArchivo(String ruta) {
-        ArrayList<String> lineas = new ArrayList<>();
-        try {
-            java.io.FileReader fr = new java.io.FileReader(ruta);
-            java.io.BufferedReader br = new java.io.BufferedReader(fr);
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                lineas.add(linea);
-            }
-            br.close();
-        } catch (Exception e) {
+            lineas.add(linea);
+            
+        }
+        
+        // cerrar el archivo
+        br.close();
+        fr.close();
+        }catch(FileNotFoundException e){
+            System.out.println("El archivo no existe");
+        }catch(IOException e){
             System.out.println("Error al leer el archivo");
         }
+
         return lineas;
     }
+
+
+
+
+    
 
     //crear método que pida escribir al usuario una linea en el archivo 
     public static void escribirArchivo(String ruta, String linea) {
