38c4a4ba8c4f01778dfda39ffab6133b6e702271
jsantis <san21153@uvg.edu.gt>
2022-04-08 10:20:15 -0600

    lectura de archivo completada

--- HDT7/src/hdt7/Archivo.java
+++ HDT7/src/hdt7/Archivo.java
@@ -4,10 +4,48 @@
  */
 package hdt7;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jsken
  */
 public class Archivo {
-    
+    //crear método que lea un archivo linea por linea y lo guarde en un arraylist
+
+    public static ArrayList<String> leerArchivo(String ruta) {
+        ArrayList<String> lineas = new ArrayList<>();
+        try {
+            java.io.FileReader fr = new java.io.FileReader(ruta);
+            java.io.BufferedReader br = new java.io.BufferedReader(fr);
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo");
+        }
+        return lineas;
+    }
+
+    //crear método que pida escribir al usuario una linea en el archivo 
+    public static void escribirArchivo(String ruta, String linea) {
+        try {
+            java.io.FileWriter fw = new java.io.FileWriter(ruta, true);
+            java.io.BufferedWriter bw = new java.io.BufferedWriter(fw);
+            bw.write(linea);
+            bw.newLine();
+            bw.close();
+        } catch (Exception e) {
+            System.out.println("Error al escribir en el archivo");
+        }
+    }
+
+
+
+   
+
+
+   
 }
