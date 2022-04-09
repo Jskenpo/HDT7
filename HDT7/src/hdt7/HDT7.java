7ec4db2ce44339ccfadd5726393fd21f2c4980c5
jsantis <san21153@uvg.edu.gt>
2022-04-08 18:50:16 -0600

    Update
    -modificar completado
    - eliminar completado
    - traducir completado
    - modificvaciones al main

--- HDT7/src/hdt7/HDT7.java
+++ HDT7/src/hdt7/HDT7.java
@@ -35,7 +35,10 @@ public class HDT7 {
             System.out.println("select the language you want to translate");
             System.out.println("1. English");
             System.out.println("2. French");
-            System.out.println("3. Salir");
+            System.out.println("3. Translate from a sentence to another language");
+            System.out.println("4. delete a node");
+            System.out.println("5. modify a node");
+            System.out.println("6. Salir");
             int opcion = Integer.parseInt(br.readLine());
 
             switch(opcion){
@@ -56,8 +59,114 @@ public class HDT7 {
                     System.out.println("Enter the word you want to translate");
                     String palabra2 = br.readLine();
                     String result2 = op.getFrenchTree().search(palabra2);
-                    
+                    if (result2 == null){
+                        System.out.println("The word doesn't exist");
+                    }
+                    else{
+                        System.out.println(result2);
+                    }
                     break;
+                case 3 :
+                  System.out.println("Ingrese el nombre del archivo a buscar");
+                  Archivo archivo = new Archivo();
+                    String nombreArchivo = br.readLine();
+                    ArrayList<String> sentence = archivo.readFile(nombreArchivo);
+                    ArrayList<String> traduccion = new ArrayList<String>();
+
+                    //separate the sentence into words and then set lower case every word
+                    for (int i = 0; i < sentence.size(); i++){
+                        String[] words = sentence.get(i).split(" ");
+                        for (int j = 0; j < words.length; j++){
+                            words[j] = words[j].toLowerCase();
+                            traduccion.add(words[i]);
+                        }
+                    }
+                    int  existe = 0;
+
+
+                    //search in the trees if the words in tradiccion exists and breaks the loop if it finds a match with one of the trees
+                    for (int i = 0; i < traduccion.size(); i++){
+                        String resultado = op.getEnglishTree().search(traduccion.get(i));
+                        String resultado2 = op.getFrenchTree().search(traduccion.get(i));
+                        if (resultado != null){
+                             existe = 1 ;
+                             break;
+                        }
+                        else if (resultado2 != null){
+                             existe = 2 ;
+                             break;
+                        }
+                        else{
+                            existe = 0;
+                            
+                        }
+                    }
+                    
+                    String total = "";
+                    if (existe == 1){
+                        for (int i = 0; i < traduccion.size(); i++){
+                            total = total + " " + op.getEnglishTree().search(traduccion.get(i));
+                        }
+                    }
+                    else if (existe == 2){
+                        for (int i = 0; i < traduccion.size(); i++){
+                            total = total + " " + op.getFrenchTree().search(traduccion.get(i));
+                        }
+                    }
+                    else{
+                            System.out.println("the words of the sentences doesn't exist");
+                    }
+                    
+                    System.out.println(total);
+
+
+
+                    break;
+                case 4 :
+                System.out.println("select the language you want to delete");
+                System.out.println("1. English");
+                System.out.println("2. French");
+                int opcion2 = Integer.parseInt(br.readLine());
+                switch(opcion2){
+                    case 1 :
+                        System.out.println("English");
+                        System.out.println("Enter the word you want to delete");
+                        String palabra3 = br.readLine();
+                        op.getEnglishTree().delete(palabra3);
+                        break;
+                    case 2 :
+                        System.out.println("French");
+                        System.out.println("Enter the word you want to delete");
+                        String palabra4 = br.readLine();
+                        op.getFrenchTree().delete(palabra4);
+                        break;
+                }
+                    
+                case 5 :
+                    //delete and adds a new node 
+                    System.out.println("select the language you want to delete");
+                    System.out.println("1. English");
+                    System.out.println("2. French");
+                    int opcion3 = Integer.parseInt(br.readLine());
+                    switch(opcion3){
+                        case 1 :
+                            System.out.println("English");
+                            System.out.println("Enter the word you want to modify");
+                            String palabra5 = br.readLine();
+                            System.out.println("put the new translation for the word");
+                            String palabra6 = br.readLine();
+                            op.getEnglishTree().delete(palabra5);
+                            op.getEnglishTree().insert(palabra5, palabra6);
+                            break;
+                        case 2 :
+                            System.out.println("French");
+                            System.out.println("Enter the word you want to modify");
+                            String palabra7 = br.readLine();
+                            System.out.println("put the new translation for the word");
+                            String palabra8 = br.readLine();
+                            op.getFrenchTree().delete(palabra7);
+                            op.getFrenchTree().insert(palabra7, palabra8);
+                    }
             }
 
         }while (!salir);
